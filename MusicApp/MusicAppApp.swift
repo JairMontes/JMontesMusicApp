50b758cd163db9e7b0952e959bad8c26ff09b152
JMontes <montes_1017@hotmail.com>
2023-06-01 19:37:50 -0600

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Jair Montes on 01/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
